518a40ba2aa3dc77985b3b5e100bb841df7072e2
‘AnastasiPr’ <ana.steshaa10@gmail.com>
2024-04-19 06:14:37 +0300

    COMMIT

---
+++ app/src/main/java/org/gaa/app/Profile.java
@@ -0,0 +1,27 @@
+package org.gaa.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.nsv.app.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Button changeLangButton = findViewById(R.id.changeLangButton);
+        changeLangButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, LanguageSelect.class));
+            }
+        });
+    }
+}
